c16ec9e7ceca80e0426826dcb4cf375b49c0fed8
Ivan Gomes <ivan@alugatech.com>
2024-08-08 03:47:46 -0300

    Incluídos consoles log

--- four-six-api/src/FourSix.WebApi/BackgroundServices/BackgroundQueueReader.cs
+++ four-six-api/src/FourSix.WebApi/BackgroundServices/BackgroundQueueReader.cs
@@ -9,19 +9,21 @@ namespace FourSix.WebApi.BackgroundServices
     public class BackgroundQueueReader : IHostedService//, IDisposable
     {
         //private Timer _timer;
-        //private readonly AmazonSQSClient _amazonSQSClient;
+        private readonly AmazonSQSClient _amazonSQSClient;
         private readonly string _endpointQueue;
         private readonly IAlteraStatusPedidoAdapter _alteraStatusPedidoAdapter;
 
         public BackgroundQueueReader(IConfiguration configuration, IAlteraStatusPedidoAdapter alteraStatusPedidoAdapter)
         {
-            
+            _amazonSQSClient = new AmazonSQSClient(RegionEndpoint.USEast1);
             _endpointQueue = configuration.GetValue<string>("Endpoints:OrdersQueue");
             _alteraStatusPedidoAdapter = alteraStatusPedidoAdapter;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
+            Console.WriteLine($"{DateTime.Now} - Iniciado background service");
+
             ReadQueue(cancellationToken);
             return Task.CompletedTask;
         }
@@ -29,6 +31,8 @@ namespace FourSix.WebApi.BackgroundServices
         public Task StopAsync(CancellationToken cancellationToken)
         {
             //_timer?.Change(Timeout.Infinite, 0);
+
+            Console.WriteLine($"{DateTime.Now} - Parado background service");
             return Task.CompletedTask;
         }
 
@@ -38,27 +42,46 @@ namespace FourSix.WebApi.BackgroundServices
             {
                 try
                 {
-                    AmazonSQSClient _amazonSQSClient = new AmazonSQSClient(RegionEndpoint.USEast1);
-
                     var response = await _amazonSQSClient.ReceiveMessageAsync(_endpointQueue, cancellationToken);
 
-                    foreach (var message in response.Messages)
+                    if (response == null || response.Messages.Count == 0)
                     {
-                        try
-                        {
-                            var pedidoModel = JsonConvert.DeserializeObject<PedidoQueueModel>(message.Body);
+                        Console.WriteLine($"{DateTime.Now} - Sem novas mensagens");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{DateTime.Now} - Mensagens obtidas: {response.Messages.Count}");
 
-                            if (pedidoModel != null)
-                                await _alteraStatusPedidoAdapter.Alterar(pedidoModel.Id, (Domain.Entities.PedidoAggregate.EnumStatusPedido)pedidoModel.StatusId);
-
-                            await _amazonSQSClient.DeleteMessageAsync(_endpointQueue, message.ReceiptHandle);
-                        }
-                        catch
+                        foreach (var message in response.Messages)
                         {
+                            try
+                            {
+                                var pedidoModel = JsonConvert.DeserializeObject<PedidoQueueModel>(message.Body);
+
+                                if (pedidoModel != null)
+                                {
+                                    Console.WriteLine($"{DateTime.Now} - Alterando pedido {pedidoModel.Id} para status {(Domain.Entities.PedidoAggregate.EnumStatusPedido)pedidoModel.StatusId}");
+
+                                    await _alteraStatusPedidoAdapter.Alterar(pedidoModel.Id, (Domain.Entities.PedidoAggregate.EnumStatusPedido)pedidoModel.StatusId);
+                                }
+
+                                Console.WriteLine($"{DateTime.Now} - Apagando mensagem");
+
+                                await _amazonSQSClient.DeleteMessageAsync(_endpointQueue, message.ReceiptHandle, cancellationToken);
+
+                                Console.WriteLine($"{DateTime.Now} - Mensagem apagada");
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.ToString());
+                            }
                         }
                     }
                 }
-                catch { }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
 
                 await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
             }
